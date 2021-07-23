2204fbce897cecce1a3c8e7d7f3f799ea7a3670d
chutipong.sae <chutipong.sae@1000320005329-0>
2021-07-23 16:39:35 +0700

    remove warning in code

---
+++ Workshop01/hello-test/src/main/java/TennisGame1.java
@@ -0,0 +1,80 @@
+import java.util.Objects;
+
+public class TennisGame1 {
+    private int m_score1 = 0;
+    private int m_score2 = 0;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame1(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (Objects.equals(playerName, "player1"))
+            m_score1 += 1;
+        else
+            m_score2 += 1;
+    }
+
+    public String getScore() {
+        StringBuilder score = new StringBuilder();
+        int tempScore;
+        if (m_score1==m_score2)
+        {
+            switch (m_score1)
+            {
+                case 0:
+                    score = new StringBuilder("Love-All");
+                    break;
+                case 1:
+                    score = new StringBuilder("Fifteen-All");
+                    break;
+                case 2:
+                    score = new StringBuilder("Thirty-All");
+                    break;
+                default:
+                    score = new StringBuilder("Deuce");
+                    break;
+
+            }
+        }
+        else if (m_score1>=4 || m_score2>=4)
+        {
+            int minusResult = m_score1-m_score2;
+            if (minusResult==1) score = new StringBuilder("Advantage player1");
+            else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
+            else if (minusResult>=2) score = new StringBuilder("Win for player1");
+            else score = new StringBuilder("Win for player2");
+        }
+        else
+        {
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) {
+                    tempScore = m_score1;
+                } else {
+                    score.append("-");
+                    tempScore = m_score2;
+                }
+                switch(tempScore)
+                {
+                    case 0:
+                        score.append("Love");
+                        break;
+                    case 1:
+                        score.append("Fifteen");
+                        break;
+                    case 2:
+                        score.append("Thirty");
+                        break;
+                    case 3:
+                        score.append("Forty");
+                        break;
+                }
+            }
+        }
+        return score.toString();
+    }
+}
