ed1876cfe02baf0ea1958082548efc47915de0c4
chutipong.sae <chutipong.sae@1000320005329-0>
2021-07-24 10:48:53 +0700

    refactor code

--- Workshop01/hello-test/src/main/java/TennisGame1.java
+++ Workshop01/hello-test/src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ import java.util.Objects;
 public class TennisGame1 {
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,69 +12,53 @@ public class TennisGame1 {
     }
 
     public void wonPoint(String playerName) {
-        if (Objects.equals(playerName, "player1"))
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (Objects.equals(playerName, this.player1Name)) {
+            m_score1++;
+        } else {
+            m_score2++;
+        }
     }
 
     public String getScore() {
-        StringBuilder score = new StringBuilder();
-        int tempScore;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = new StringBuilder("Love-All");
-                    break;
-                case 1:
-                    score = new StringBuilder("Fifteen-All");
-                    break;
-                case 2:
-                    score = new StringBuilder("Thirty-All");
-                    break;
-                default:
-                    score = new StringBuilder("Deuce");
-                    break;
+        String[] scoreResults = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        // All
+        if (isAll()) {
+            return scoreResults[m_score1] + "-" + "All";
+        }
 
-            }
+        // Deuce
+        if (isDeuce()) {
+            // Deuce
+            return "Deuce";
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score = new StringBuilder("Advantage player1");
-            else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
-            else if (minusResult>=2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+
+        int diffScore = m_score1 - m_score2;
+
+        // Win
+        if ((m_score1 >= 4 || m_score2 >= 4) && Math.abs(diffScore) >= 2) {
+            StringBuilder score = new StringBuilder();
+            if (diffScore >= 2) score = new StringBuilder("Win for ").append(this.player1Name);
+            else score = new StringBuilder("Win for ").append(this.player2Name);
+            return score.toString();
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) {
-                    tempScore = m_score1;
-                } else {
-                    score.append("-");
-                    tempScore = m_score2;
-                }
-                switch(tempScore)
-                {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
+
+        // Advantage
+        if ((m_score1 >= 4 || m_score2 >= 4) && Math.abs(diffScore) == 1) {
+            StringBuilder score = new StringBuilder();
+            if (diffScore == 1) score = new StringBuilder("Advantage ").append(this.player1Name);
+            else if (diffScore == -1) score = new StringBuilder("Advantage ").append(this.player2Name);
+            return score.toString();
         }
-        return score.toString();
+
+        // Normal game
+        return scoreResults[m_score1] + "-" + scoreResults[m_score2];
+    }
+
+    private boolean isDeuce() {
+        return m_score1 == m_score2 && m_score1 > 2;
+    }
+
+    private boolean isAll() {
+        return m_score1 == m_score2 && m_score1 <= 2;
     }
 }
