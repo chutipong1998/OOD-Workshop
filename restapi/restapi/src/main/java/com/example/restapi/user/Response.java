5bdbefac0b1cd9dbfa5aba6b2dff861a3c6f4c2a
chutipong.sae <chutipong.sae@1000320005329-0>
2021-07-24 16:34:38 +0700

    add setter

--- restapi/restapi/src/main/java/com/example/restapi/user/Response.java
+++ restapi/restapi/src/main/java/com/example/restapi/user/Response.java
@@ -10,32 +10,64 @@ public class Response{
 	private String email;
 	private String username;
 
+	public void setWebsite(String website){
+		this.website = website;
+	}
+
 	public String getWebsite(){
 		return website;
+	}
+
+	public void setAddress(Address address){
+		this.address = address;
 	}
 
 	public Address getAddress(){
 		return address;
 	}
 
+	public void setPhone(String phone){
+		this.phone = phone;
+	}
+
 	public String getPhone(){
 		return phone;
+	}
+
+	public void setName(String name){
+		this.name = name;
 	}
 
 	public String getName(){
 		return name;
 	}
 
+	public void setCompany(Company company){
+		this.company = company;
+	}
+
 	public Company getCompany(){
 		return company;
+	}
+
+	public void setId(int id){
+		this.id = id;
 	}
 
 	public int getId(){
 		return id;
 	}
 
+	public void setEmail(String email){
+		this.email = email;
+	}
+
 	public String getEmail(){
 		return email;
+	}
+
+	public void setUsername(String username){
+		this.username = username;
 	}
 
 	public String getUsername(){
